a0030789422c26daa53caf7e832fe4eab2d0654a
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-08-02 06:41:40 -0300

    feat: estrutura inicial

---
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/AutoPecasDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.pecasDelivery.AutoPecas_Delivery_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPecasDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPecasDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pecasDelivery/AutoPecas_Delivery_API/AutoPecasDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pecasDelivery.AutoPecas_Delivery_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoPecasDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
