52a109808f01460704cc356cebc8ee1f1d35f6cd
MatheusBairrosS <matheusbs123zt@gmail.com>
2024-08-10 19:17:40 -0300

    fix: Security Configuration

--- src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/security/config/SecurityConfiguration.java
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/security/config/SecurityConfiguration.java
@@ -1,10 +1,20 @@
 package com.pecasDelivery.AutoPecas_Delivery_API.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import com.pecasDelivery.AutoPecas_Delivery_API.security.authentication.UsuarioAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+	
+	@Autowired
+    private UsuarioAuthenticationFilter usuarioAuthenticationFilter;
 
+	public static final String[] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
+            "/users/login",
+            "/users"
+    };
 }
