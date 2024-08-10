cbcef67e7dadd1ec602222b33c9f5a854abd2a2b
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-08-10 17:51:53 -0300

    feat: UsuarioRepository findbyemail

---
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.pecasDelivery.AutoPecas_Delivery_API.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pecasDelivery.AutoPecas_Delivery_API.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
+
+	Optional<Usuario> findByEmail(String email);
+}
