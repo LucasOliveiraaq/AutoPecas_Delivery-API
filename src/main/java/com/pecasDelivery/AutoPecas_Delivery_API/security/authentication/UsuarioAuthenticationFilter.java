8ee8df0a39c6c056ed2cc8d429b1ceb9a56322bc
MatheusBairrosS <matheusbs123zt@gmail.com>
2024-08-10 19:20:03 -0300

    fix: UsuarioAuthenticationFilter

--- src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/security/authentication/UsuarioAuthenticationFilter.java
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/security/authentication/UsuarioAuthenticationFilter.java
@@ -1,12 +1,20 @@
 package com.pecasDelivery.AutoPecas_Delivery_API.security.authentication;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.pecasDelivery.AutoPecas_Delivery_API.model.Usuario;
 import com.pecasDelivery.AutoPecas_Delivery_API.repositories.UsuarioRepository;
+import com.pecasDelivery.AutoPecas_Delivery_API.security.config.SecurityConfiguration;
+import com.pecasDelivery.AutoPecas_Delivery_API.security.details.UserDetailsImpl;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,7 +32,39 @@ public class UsuarioAuthenticationFilter extends OncePerRequestFilter{
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		
+		if(verificaEndPointIsNotPublic(request)) {
+			String token = recuperarToken(request);
+			if(token != null) {
+				String subject = jwtTokenService.getSubjectFromToken(token);
+				Optional<Usuario> optionalUser = usuarioRepository.findByEmail(subject);
+				if(optionalUser.isPresent()) {
+					Usuario user  = optionalUser.get();
+					UserDetailsImpl userDetails = new UserDetailsImpl(user);
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    System.out.println("Authentication successful for user: " + user.getEmail());
+                    
+				} else {
+					System.out.println("User not found for subject: " + subject); 
+				}
+			} else {
+				System.out.println("Token is missing in the request.");
+			}
+		}
+		filterChain.doFilter(request, response);
+	}
+	
+	private boolean verificaEndPointIsNotPublic(HttpServletRequest request)  {
+		String requestURI = request.getRequestURI();
+		return !Arrays.asList(SecurityConfiguration.ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED).contains(requestURI);
+	}
+	
+	private String recuperarToken(HttpServletRequest request) {
+		String authorizationHeader = request.getHeader("Authorization");
+		if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+			return authorizationHeader.replace("Bearer ", "").trim();
+		}
+		return null;
 	}
 
 }
