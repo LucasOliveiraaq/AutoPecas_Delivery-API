b81dd3b62cac83c3c28fc8c9c85a2b2f59101d20
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-08-10 12:15:08 -0300

    fix: Usuario users_roles

--- src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/model/Usuario.java
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/model/Usuario.java
@@ -1,14 +1,20 @@
 package com.pecasDelivery.AutoPecas_Delivery_API.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -35,6 +41,11 @@ public class Usuario {
 	
 	@Column(nullable = false, length = 255)
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+	@JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"), 
+									 inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<Role> roles;
 	
 	@Column(nullable = false, updatable = false)
 	@CreationTimestamp
