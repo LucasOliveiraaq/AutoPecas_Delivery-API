13edcd262fba260be1fa0d6c62dd6d6ff44df0ba
LucasOliveiraaq <lucasp.oliveira122@gmail.com>
2024-08-10 12:23:37 -0300

    feat: Security

---
+++ src/main/java/com/pecasDelivery/AutoPecas_Delivery_API/security/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.pecasDelivery.AutoPecas_Delivery_API.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+}
